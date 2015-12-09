a83c11e2e4b25ed197fd26ec08ee8828adcb9b35
Mike Puzio <mcpuzio@gmail.com>
2015-12-09 08:38:37 -0500

    Added portfolio1, 2, and js files.

---
+++ mpuzio_portfolioproject/portfolio1.js
@@ -0,0 +1,15 @@
+$(document).ready(
+	function() {
+		$("body").click(
+		function(){
+			$("#wrapper1").slideUp();
+
+			$("h1").fadeIn(2000);
+				});
+		$("p").click(function(){
+        $(this).hide();
+    });
+
+	});
+
+	});
